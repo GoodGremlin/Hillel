a2f1361fd5577a86294cec4079ab91cf669d4763
Valeriy <33179237+GoodGremlin@users.noreply.github.com>
2024-06-11 19:24:31 +0200

    homework 5.2

---
+++ main/homework_5/homework_5_2.py
@@ -0,0 +1,37 @@
+operators = '+-*/'
+
+isContinue = True
+
+operator = ''
+
+while isContinue:
+    expression = input('Enter a example: ')
+    result = ''
+
+    for char in expression:
+        if char in operators:
+            operator = char
+            break
+
+    if operator:
+        number1, number2 = map(int, expression.split(operator))
+
+        if operator == '+':
+            result = number1 + number2
+        elif operator == '-':
+            result = number1 - number2
+        elif operator == '*':
+            result = number1 * number2
+        elif operator == '/':
+            if number2 != 0:
+                result = number1 / number2
+            else:
+                result = 'Error: Division by zero'
+        else:
+            result = 'Error: Invalid operator'
+
+        print(result)
+
+        answerToContinue = input('Continue? [y/n]: ')
+
+        isContinue = answerToContinue.lower() == 'y'
