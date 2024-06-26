dc9334d370e52bfd7ed8c7dc97cb8a9e64033490
Valeriy <33179237+GoodGremlin@users.noreply.github.com>
2024-06-26 15:02:33 +0200

    homework 8

--- main/homework_8/homework_8_1.py
+++ main/homework_8/homework_8_1.py
@@ -1,13 +1,17 @@
 def add_one(some_list):
+    if some_list[-1] < 9:
+        some_list[-1] += 1
+        return some_list
+
+    some_list[-1] += 1
     for i in range(len(some_list) - 1, -1, -1):
-        some_list[i] += 1
         if some_list[i] < 10:
             return some_list
         some_list[i] = 0
+        if i == 0:
+            some_list.append(0)
 
-    some_list.insert(0, 1)
     return some_list
-
 
 assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
 assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
